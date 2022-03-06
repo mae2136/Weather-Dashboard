6f89e4ac12318766801101e7aa519a80cdaa38b1
Miguel Escobar <mae2136@gmail.com>
2022-03-06 17:09:00 -0600

    index.html: - Add element ids.
    script.js:
    - Add event listener to search button.
    - Ensure search term can be used properly.

--- Assests/script.js
+++ Assests/script.js
@@ -1,12 +1,25 @@
 // OpenWeather API key
 var key = '0fd60888fa5456692e37096091f275a9';
 var formEl = document.querySelector(`#searchForm`);
+var searchBtn = document.getElementById(`searchBtn`);
+
 console.log(formEl);
 
 // Grab search term from user form
+searchBtn.addEventListener('click', getWeather);
 
+function getWeather(event) {
+    event.preventDefault();
+    console.log("Searched");
+    var searchCity = document.getElementById(`searchText`).value;
+    console.log(searchCity)
+    searchWeather(searchCity);
+}
 
 // Fetch search term to OpenWeather API
+function searchWeather(city) {
+    console.log(city);
+}
 // Parse response
 // Display weather conditions in the city for today
 // Includes temp, wind, humidity, UV index
