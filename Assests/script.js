8c89c2a914dfc6d4fd1566066ecd314370b04a8d
Miguel Escobar <mae2136@gmail.com>
2022-03-06 14:42:26 -0600

    index.html: - Add search form elements.
    sciprt.js:
    - Add pseudo code.
    - Add variables for API key and form element.

---
+++ Assests/script.js
@@ -0,0 +1,15 @@
+// OpenWeather API key
+var key = '0fd60888fa5456692e37096091f275a9';
+var formEl = document.querySelector(`#searchForm`);
+console.log(formEl);
+
+// Grab search term from user form
+
+
+// Fetch search term to OpenWeather API
+// Parse response
+// Display weather conditions in the city for today
+// Includes temp, wind, humidity, UV index
+// UV index is color coded for favorable, moderate, severe
+// Display 5 day forecast via 5 cards
+// Save user search term in a new input at end of the form
