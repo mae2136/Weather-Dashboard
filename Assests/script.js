7e373b1ab0db65143ecbb4515dfd585f3289e83a
Miguel Escobar <mae2136@gmail.com>
2022-03-12 16:53:42 -0600

    - Update html to add bootstrap functionality. - Update html to add containers and rows to display data. - Update CSS to add font families. - Update JS to pull weather data from API.

--- Assests/script.js
+++ Assests/script.js
@@ -1,49 +1,41 @@
 // OpenWeather API key
-var key = '0fd60888fa5456692e37096091f275a9';
+var key = `0fd60888fa5456692e37096091f275a9`;
 var formEl = document.querySelector(`#searchForm`);
 var searchBtn = document.getElementById(`searchBtn`);
-
-console.log(formEl);
-
+var searchCity = document.getElementById(`searchText`)
 // Grab search term from user form
 searchBtn.addEventListener('click', getWeather);
 
 function getWeather(event) {
     event.preventDefault();
-    console.log("Searched");
-    var searchCity = document.getElementById(`searchText`).value;
-    searchWeather(searchCity);
+    // Store searched city name in variable
+    var city = searchCity.value;
+    // Clear search form
+    searchCity.value = "";
+    searchWeather(city);
+
 }
 
 // Fetch search term to OpenWeather API
 function searchWeather(city) {
-    console.log(city);
-    // Grab lat and longitude first?
-    var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=` + city + `&limit=1&appid=` + key;
-    fetch(geoUrl)
-        .then(function (response) {
-            if (response.status === 200) {
-                // Parse response
-                return response.json().then(renderWeather(response));;
-            } else {
-                alert('Error: ' + response.statusText);
-            }
-        });
-    var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=` + key;
-    console.log(geoUrl);
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    console.log(apiUrl);
     fetch(apiUrl)
         .then(function (response) {
             if (response.status === 200) {
                 // Parse response
-                return response.json().then(renderWeather(response));;
+                response.json()
+                    .then(function (weatherData) {
+                       renderWeather(weatherData); 
+                    });
             } else {
                 alert('Error: ' + response.statusText);
             }
-        })
+        });
 }
 // Display weather conditions in the city for today
-function renderWeather(response) {
-    console.log(response);
+function renderWeather(weatherData) {
+    console.log(weatherData);
 }
 // Includes temp, wind, humidity, UV index
 // UV index is color coded for favorable, moderate, severe
