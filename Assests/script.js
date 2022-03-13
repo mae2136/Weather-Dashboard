9db5792de134d39fabbbcfbe0900102ff7fb1cbc
Miguel Escobar <mae2136@gmail.com>
2022-03-12 18:01:34 -0600

    - Hide Data information display from html. - Move weather data for today elements to javascript. - Display weather information (minus UV index).

--- Assests/script.js
+++ Assests/script.js
@@ -2,7 +2,9 @@
 var key = `0fd60888fa5456692e37096091f275a9`;
 var formEl = document.querySelector(`#searchForm`);
 var searchBtn = document.getElementById(`searchBtn`);
-var searchCity = document.getElementById(`searchText`)
+var searchCity = document.getElementById(`searchText`);
+var currentWeatherEl = document.getElementById(`currentWeather`);
+var weatherCards = document.getElementById(`weatherCards`);
 // Grab search term from user form
 searchBtn.addEventListener('click', getWeather);
 
@@ -18,7 +20,7 @@ function getWeather(event) {
 
 // Fetch search term to OpenWeather API
 function searchWeather(city) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
     console.log(apiUrl);
     fetch(apiUrl)
         .then(function (response) {
@@ -26,7 +28,7 @@ function searchWeather(city) {
                 // Parse response
                 response.json()
                     .then(function (weatherData) {
-                       renderWeather(weatherData); 
+                        renderWeather(weatherData);
                     });
             } else {
                 alert('Error: ' + response.statusText);
@@ -36,8 +38,27 @@ function searchWeather(city) {
 // Display weather conditions in the city for today
 function renderWeather(weatherData) {
     console.log(weatherData);
+    console.log(currentWeatherEl);
+    var icon = weatherIcon(weatherData);
+    currentWeatherEl.innerHTML = `
+        <h2 class="p-2">${weatherData.name} (${weatherData.dt}) <img class="" src="${icon}" alt="weatherIcon"></h2>
+        <h3 class="p-2">Temp: ${weatherData.main.temp} \xB0F</h3>
+        <h3 class="p-2">Wind: ${weatherData.wind.speed} MPH</h3
+        ><h3 class="p-2">Humidity: ${weatherData.main.humidity} %</h3>
+        <h3 class="p-2">UV Index: ${weatherData.main.temp}</h3>`;
+    currentWeatherEl.classList.remove(`d-none`);
+    weatherCards.innerHTML = "";
 }
 // Includes temp, wind, humidity, UV index
+// Convert temperature from Kelvins to F
+// Convert time (dt) from Uni, UTC to local time
+// 
+
+// Icon solution found at https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+function weatherIcon(weatherData) {
+    var iconUrl = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
+    return iconUrl
+}
 // UV index is color coded for favorable, moderate, severe
 // Display 5 day forecast via 5 cards
 // Save user search term in a new input at end of the form
